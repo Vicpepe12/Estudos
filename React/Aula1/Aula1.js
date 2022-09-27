2c49cee7950b5b36b567f3739a55bb3ae0501792
Vicpepe12 <victorafonseca9@gmail.com>
2022-09-27 12:58:47 -0300

    teste

--- React/Aula1/Aula1.js
+++ React/Aula1/Aula1.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// Introdução a JSX
+function escreveNome(nome){
+    return nome.primeiroNome + '' + nome.ultimoNome;
+}
 
-const teste = 'teste'
-// testetesteteste
+const nome = 'Denis Jr.'
+const element = <h1>Olá, {nome}!!</h1> //É possível colocar elementos de sintaxe de HTML dentro do javascript
+
+
+
+const nomeDoDenis = {
+    primeiroNome: 'Denis',
+    ultimoNome: 'Jr.'
+}
+
+let element2 = <h1>Olá, {escreveNome(nomeDoDenis)}!</h1>
+
+teste.innerHTML += "teste"
+
+console.log('teste')
+
+// comentário
